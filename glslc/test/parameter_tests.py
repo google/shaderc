9ce65fd86a925024940fcec46c1d69cf434a6f9a
qining <qining@google.com>
2016-02-01 12:15:06 -0500

    update the help message about --target-env

--- glslc/test/parameter_tests.py
+++ glslc/test/parameter_tests.py
@@ -71,7 +71,8 @@ Options:
   -S                Only run preprocess and compilation steps.
   --target-env=<environment>
                     Set the target shader environment, and the semantics
-                    of warnings and errors.
+                    of warnings and errors. Valid values are 'opengl',
+                    'opengl_compat' and 'vulkan'. The default value is 'vulkan'.
   -w                Suppresses all warning messages.
   -Werror           Treat all warnings as errors.
   -working-directory <dir>
