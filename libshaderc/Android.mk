9a35bcf34bcdc23f7b2dcc082516521ca183034d
Lei Zhang <antiagainst@google.com>
2016-04-21 14:57:23 -0400

    Add new dependencies for assembling support into Android.mk.

--- libshaderc/Android.mk
+++ libshaderc/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE:=shaderc
 LOCAL_EXPORT_C_INCLUDES:=$(LOCAL_PATH)/include
 LOCAL_SRC_FILES:=src/shaderc.cc
 LOCAL_C_INCLUDES:=$(LOCAL_PATH)/include
-LOCAL_STATIC_LIBRARIES=shaderc_util
+LOCAL_STATIC_LIBRARIES=shaderc_util SPIRV-Tools
 LOCAL_CXXFLAGS:=-std=c++11 -fno-exceptions -fno-rtti
 LOCAL_EXPORT_CPPFLAGS:=-std=c++11
 LOCAL_EXPORT_LDFLAGS:=-latomic
--- libshaderc_util/Android.mk
+++ libshaderc_util/Android.mk
@@ -10,6 +10,7 @@ LOCAL_SRC_FILES:=src/compiler.cc \
 		src/message.cc \
 		src/resources.cc \
 		src/shader_stage.cc \
+		src/spirv_tools_wrapper.cc \
 		src/version_profile.cc
 LOCAL_STATIC_LIBRARIES:=glslang SPIRV-Tools
 LOCAL_C_INCLUDES:=$(LOCAL_PATH)/include
