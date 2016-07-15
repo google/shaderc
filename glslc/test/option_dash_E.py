caa3ca51d9713659642af4b4a6e6aaaf2716291e
Lei Zhang <antiagainst@google.com>
2016-07-15 13:56:30 -0400

    Address pep8 warnings about in option_dash_E.py.

--- glslc/test/option_dash_E.py
+++ glslc/test/option_dash_E.py
@@ -22,24 +22,27 @@ class TestDashCapENoDefs(expect.StdoutMatch):
     """Tests -E without any defines."""
 
     shader = FileShader('#version 140\nvoid main(){}', '.vert')
-    expected_stdout = '#version 140\nvoid main(){ }\n';
+    expected_stdout = '#version 140\nvoid main(){ }\n'
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEGlslFileAccepted(expect.StdoutMatch):
     """Tests -E if we provide a .glsl file without an explicit stage."""
 
     shader = FileShader('#version 140\nvoid main(){}', '.glsl')
-    expected_stdout = '#version 140\nvoid main(){ }\n';
+    expected_stdout = '#version 140\nvoid main(){ }\n'
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapESingleDefine(expect.StdoutMatch):
     """Tests -E with command-line define."""
 
     shader = FileShader('#version 140\nvoid main(){ int a = X; }', '.vert')
-    expected_stdout = '#version 140\nvoid main(){ int a = 4;}\n';
+    expected_stdout = '#version 140\nvoid main(){ int a = 4;}\n'
     glslc_args = ['-DX=4', '-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEExpansion(expect.StdoutMatch):
@@ -57,8 +60,9 @@ void main() {
 void main(){
   int a = 4;
 }
-''';
+'''
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEFunctionMacro(expect.StdoutMatch):
@@ -76,8 +80,9 @@ void main() {
 void main(){
   int a = 4 + 1;
 }
-''';
+'''
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEPragma(expect.StdoutMatch):
@@ -93,8 +98,9 @@ void main() {
 #pragma optimize(off)
 void main(){
 }
-''';
+'''
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEExtension(expect.StdoutMatch):
@@ -110,8 +116,9 @@ void main() {
 #extension foo : require
 void main(){
 }
-''';
+'''
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapELine(expect.StdoutMatch):
@@ -135,8 +142,9 @@ void main() {
 
 void main(){
 }
-''';
+'''
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEError(expect.ErrorMessage):
@@ -151,11 +159,12 @@ void main() {
 }
 ''', '.vert')
 
-    expected_error= [
+    expected_error = [
         shader, ':3: error: \'#error\' : This is an error\n',
         '1 error generated.\n']
 
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEStdin(expect.StdoutMatch):
@@ -169,8 +178,9 @@ void main() {
     expected_stdout = '''#version 140
 void main(){
 }
-''';
+'''
     glslc_args = ['-E', '-fshader-stage=vertex', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEStdinDoesNotRequireShaderStage(expect.StdoutMatch):
@@ -185,8 +195,9 @@ void main() {
     expected_stdout = '''#version 140
 void main(){
 }
-''';
+'''
     glslc_args = ['-E', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEMultipleFiles(expect.StdoutMatch):
@@ -206,8 +217,9 @@ void main(){
 #version 140
 void function(){
 }
-''';
+'''
     glslc_args = ['-E', '-fshader-stage=vertex', shader, shader2]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEMultipleFilesWithoutStage(expect.StdoutMatch):
@@ -228,7 +240,7 @@ void main(){
 #version 140
 void function(){
 }
-''';
+'''
     glslc_args = ['-E', shader, shader2]
 
 
@@ -240,36 +252,40 @@ class TestDashCapEOutputFile(expect.SuccessfulReturn, expect.ValidFileContents):
 void function() {
 }
 ''', '.vert')
-    expected_file_contents='''#version 140
+    expected_file_contents = '''#version 140
 void function(){
 }
 '''
-    target_filename='foo'
+    target_filename = 'foo'
     glslc_args = ['-E', shader, '-ofoo']
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEWithS(expect.StdoutMatch):
     """Tests -E in the presence of -S."""
 
     shader = FileShader('#version 140\nvoid main(){}', '.vert')
-    expected_stdout = '#version 140\nvoid main(){ }\n';
+    expected_stdout = '#version 140\nvoid main(){ }\n'
     glslc_args = ['-E', '-S', shader]
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestMultipileDashCapE(expect.StdoutMatch):
     """Tests that using -E multiple times works."""
 
     shader = FileShader('#version 140\nvoid main(){}', '.vert')
-    expected_stdout = '#version 140\nvoid main(){ }\n';
+    expected_stdout = '#version 140\nvoid main(){ }\n'
     glslc_args = ['-E', '-E', shader, '-E']
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEAfterFile(expect.StdoutMatch):
     """Tests that using -E after the filename also works."""
 
     shader = FileShader('#version 140\nvoid main(){}', '.vert')
-    expected_stdout = '#version 140\nvoid main(){ }\n';
+    expected_stdout = '#version 140\nvoid main(){ }\n'
     glslc_args = [shader, '-E']
+
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEWithDashC(expect.StdoutMatch):
@@ -315,9 +331,10 @@ void main() {
         '4 errors generated.\n']
     glslc_args = ['-E', shader]
 
+
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEStdinErrors(expect.ErrorMessage):
-    """Tests to make sure -E outputs error messages correctly for stdin input."""
+    """Tests that -E outputs error messages correctly for stdin input."""
 
     shader = StdinShader('''#version 310 es
 #extension s enable // missing :
@@ -345,15 +362,16 @@ void main(){
 
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEIgnoresTargetEnvOpengl(expect.StdoutMatch):
-    """Tests to make sure --target-env=opengl is ignored when -E is set."""
+    """Tests that --target-env=opengl is ignored when -E is set."""
 
     shader = FileShader(opengl_compat_frag_shader(), '.frag')
     expected_stdout = opengl_compat_frag_shader()
     glslc_args = ['-E', '--target-env=opengl', shader]
 
+
 @inside_glslc_testsuite('OptionCapE')
 class TestDashCapEIgnoresTargetEnvOpenglCompat(expect.StdoutMatch):
-    """Tests to make sure --target-env=opengl_compat is ignored when -E is set."""
+    """Tests that --target-env=opengl_compat is ignored when -E is set."""
 
     shader = FileShader(opengl_compat_frag_shader(), '.frag')
     expected_stdout = opengl_compat_frag_shader()
