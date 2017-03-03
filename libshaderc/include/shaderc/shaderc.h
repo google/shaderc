cb4f0f6261b2f000126476200e7234a5f34895df
David Neto <dneto@google.com>
2017-03-03 18:59:11 -0500

    Describe includer error convention in the struct.
    Related to https://github.com/google/shaderc/pull/309

--- libshaderc/include/shaderc/shaderc.h
+++ libshaderc/include/shaderc/shaderc.h
@@ -287,9 +287,11 @@ typedef struct shaderc_include_result {
   // in the sense that it should be a unique name in the context of the
   // includer.  For example, if the includer maps source names to files in
   // a filesystem, then this name should be the absolute path of the file.
+  // For a failed inclusion, this string is empty.
   const char* source_name;
   size_t source_name_length;
-  // The text contents of the source file.
+  // The text contents of the source file in the normal case.
+  // For a failed inclusion, this contains the error message.
   const char* content;
   size_t content_length;
   // User data to be passed along with this request.
