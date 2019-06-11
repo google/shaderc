07d4f12dbde90bc216c7cf3c8ab4728c4039a721
Ryan Harrison <rharrison@google.com>
2019-06-11 11:11:42 -0400

    Test PR, plz ignore

--- spvc/test/run_spirv_cross_tests.py
+++ spvc/test/run_spirv_cross_tests.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# I am a non-functional change to create a diff
 
 """Run the spirv-cross tests on spvc."""
 
