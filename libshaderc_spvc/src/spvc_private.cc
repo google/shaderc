e0be1630e7704d0121676e485dd16b9c17ac0b41
SarahM0 <sarahmashay@google.com>
2019-10-01 13:30:22 -0400

    review comments and add missing option to CMAKE file

--- libshaderc_spvc/src/spvc_private.cc
+++ libshaderc_spvc/src/spvc_private.cc
@@ -292,44 +292,47 @@ shaderc_spvc_compilation_result_t generate_hlsl_shader(
 
   std::unique_ptr<spirv_cross::CompilerHLSL> compiler;
 
-  // spvc is under development, for now run spirv-cross compiler by default
-  // TODO (sarahM0): change the default to spvc when it's done
-  #if ENABLE_SPVC_PARSER
-    spvtools::Optimizer opt(options->source_env);
-    opt.SetMessageConsumer(std::bind(
-        consume_spirv_tools_message, result, std::placeholders::_1,
-        std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
+// spvc IR generation is under development, for now run spirv-cross
+// compiler(ENABLE_SPVC_PARSER is OFF by default)
+// TODO (sarahM0): change the default to spvc IR generation when it's done
+#if ENABLE_SPVC_PARSER
+  spvtools::Optimizer opt(options->source_env);
+  opt.SetMessageConsumer(std::bind(
+      consume_spirv_tools_message, result, std::placeholders::_1,
+      std::placeholders::_2, std::placeholders::_3, std::placeholders::_4));
 
-    spirv_cross::ParsedIR ir;
-    ir.spirv = std::vector<uint32_t>(source, source + source_len);
+  spirv_cross::ParsedIR ir;
+  ir.spirv = std::vector<uint32_t>(source, source + source_len);
 
-    opt.RegisterPass(
-        spvtools::Optimizer::PassToken(std::unique_ptr<spvtools::opt::Pass>(
-            reinterpret_cast<spvtools::opt::Pass*>(
-                new spvtools::opt::SpvcIrPass(ir)))));
+  opt.RegisterPass(
+      spvtools::Optimizer::PassToken(std::unique_ptr<spvtools::opt::Pass>(
+          reinterpret_cast<spvtools::opt::Pass*>(
+              new spvtools::opt::SpvcIrPass(ir)))));
 
-    if (!opt.Run(source, source_len, &result->binary_output)) {
-      result->messages.append(
-          "Transformations between source and target "
-          "execution environments failed.\n");
-      result->status = shaderc_compilation_status_transformation_error;
-      compiler.reset(new (std::nothrow) spirv_cross::CompilerHLSL(ir));
-    }
+  if (!opt.Run(source, source_len, &result->binary_output)) {
+    result->messages.append(
+        "Transformations between source and target "
+        "execution environments failed.\n");
+    result->status = shaderc_compilation_status_transformation_error;
+    compiler.reset(new (std::nothrow) spirv_cross::CompilerHLSL(ir));
+  }
 
-    result->messages.append("Tried to do a temporary pass.\n");
+  result->messages.append(
+      "Expected failure due to incomplete spvc IR generation "
+      "implementation.\n");
+  result->status = shaderc_compilation_status_compilation_error;
+  return result;
+
+#else
+  compiler.reset(new (std::nothrow)
+                     spirv_cross::CompilerHLSL(source, source_len));
+  if (!compiler) {
+    result->messages.append(
+        "Unable to initialize SPIRV-Cross HLSL compiler.\n");
     result->status = shaderc_compilation_status_compilation_error;
     return result;
-
-  #else
-    compiler.reset(new (std::nothrow)
-                       spirv_cross::CompilerHLSL(source, source_len));
-    if (!compiler) {
-      result->messages.append(
-          "Unable to initialize SPIRV-Cross HLSL compiler.\n");
-      result->status = shaderc_compilation_status_compilation_error;
-      return result;
-    }
-  #endif
+  }
+#endif
 
   compiler->set_common_options(options->glsl);
   compiler->set_hlsl_options(options->hlsl);
