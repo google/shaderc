2c3e87875cec64f7581aa7cb7c8d0d253955ddc0
Ryan Harrison <rharrison@google.com>
2019-12-04 14:15:17 -0500

    Remove unused variable from C++ API

--- libshaderc_spvc/include/spvc/spvc.hpp
+++ libshaderc_spvc/include/spvc/spvc.hpp
@@ -355,9 +355,9 @@ class Context {
   // Set spirv_cross decoration (added for HLSL support in Dawn)
   // Given an id, decoration and argument, the decoration flag on the id is set,
   // assuming id is valid.
-  shaderc_compilation_status SetDecoration(
-      const shaderc_spvc_context_t context, uint32_t id,
-      shaderc_spvc_decoration decoration, uint32_t argument){
+  shaderc_compilation_status SetDecoration(uint32_t id,
+                                           shaderc_spvc_decoration decoration,
+                                           uint32_t argument) {
     return shaderc_spvc_set_decoration(context_.get(), id, decoration, argument);
   }
 
