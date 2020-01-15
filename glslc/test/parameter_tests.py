cdac124e8372b933c91cfdcfc852ad4dd63f8967
David Neto <dneto@google.com>
2020-01-15 12:51:13 -0800

    Support Vulkan 1.2 (#958)
    Compiling for Vulkan 1.2 defaults to using SPIR-V 1.5.
    
    Depends on Vulkan 1.2 support in Glslang, and SPIRV-Tools
    Depends on SPIR-V 1.5 support in SPIRV-Headers
    
    Update DEPS for SPIRV-Tools and Glslang

--- glslc/test/parameter_tests.py
+++ glslc/test/parameter_tests.py
@@ -157,6 +157,7 @@ Options:
                     the client version.  Values are:
                         vulkan1.0       # The default
                         vulkan1.1
+                        vulkan1.2
                         vulkan          # Same as vulkan1.0
                         opengl4.5
                         opengl          # Same as opengl4.5
@@ -165,7 +166,8 @@ Options:
                     module.  The default is the highest version of SPIR-V
                     required to be supported for the target environment.
                     For example, default for vulkan1.0 is spv1.0, and
-                    the default for vulkan1.1 is spv1.3.
+                    the default for vulkan1.1 is spv1.3,
+                    the default for vulkan1.2 is spv1.5.
                     Values are:
                         spv1.0, spv1.1, spv1.2, spv1.3, spv1.4, spv1.5
   --version         Display compiler version information.
