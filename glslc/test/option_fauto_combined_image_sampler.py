2d52bb940b0500da357af49ab5366dd82ff00dfe
David Neto <dneto@google.com>
2021-04-22 12:21:27 -0400

    Fixed and reformatted -fauto-combine-image-sampler test
    - Added I/O locations to the GLSL example, to make it compile without
      the extra -fauto-map-locations option
    - Removed the stray exit(17) that was causing the module to not load.
    - Reformatted the expected SPIR-V disassembly string so it's easier
      to read. Use a multi-line string.

--- glslc/test/option_fauto_combined_image_sampler.py
+++ glslc/test/option_fauto_combined_image_sampler.py
@@ -18,8 +18,8 @@ from placeholder import FileShader
 
 # A GLSL shader with a separate sampler and texture2D object
 GLSL_SHADER_SEPARATE_IMAGE_SAMPLER = """#version 460
-  in vec2 in_UV;
-  out vec4 out_Color;
+  layout (location=0) in vec2 in_UV;
+  layout (location=0) out vec4 out_Color;
   layout (set=0,binding=0) uniform sampler u_Sampler;
   layout (set=0,binding=0) uniform texture2D u_Tex;
   void main() {
@@ -39,12 +39,12 @@ HLSL_SHADER_SEPARATE_IMAGE_SAMPLER = """
 @inside_glslc_testsuite('OptionFAutoCombinedImageSampler')
 class FAutoCombinedImageSamplerCheckGLSL(expect.ValidAssemblyFileWithSubstr):
     """Tests that the compiler combines GLSL sampler and texture2D objects."""
-
-    exit(17)
-
     shader = FileShader(GLSL_SHADER_SEPARATE_IMAGE_SAMPLER, '.frag')
     glslc_args = ['-S', '-fauto-combined-image-sampler', shader]
-    expected_assembly_substr = "%11 = OpTypeSampledImage %10\n%_ptr_UniformConstant_11 = OpTypePointer UniformConstant %11\n      %u_Tex = OpVariable %_ptr_UniformConstant_11 UniformConstant"
+    expected_assembly_substr = """%10 = OpTypeImage %float 2D 0 0 0 1 Unknown
+         %11 = OpTypeSampledImage %10
+%_ptr_UniformConstant_11 = OpTypePointer UniformConstant %11
+      %u_Tex = OpVariable %_ptr_UniformConstant_11 UniformConstant"""
 
 @inside_glslc_testsuite('OptionFAutoCombinedImageSampler')
 class FAutoCombinedImageSamplerCheckHLSL(expect.ValidAssemblyFileWithSubstr):
@@ -52,5 +52,8 @@ class FAutoCombinedImageSamplerCheckHLSL(expect.ValidAssemblyFileWithSubstr):
 
     shader = FileShader(HLSL_SHADER_SEPARATE_IMAGE_SAMPLER, '.hlsl')
     glslc_args = ['-S', '-fshader-stage=frag', '-fentry-point=Frag', '-fauto-combined-image-sampler', shader]
-    expected_assembly_substr = "%15 = OpTypeSampledImage %14\n%_ptr_UniformConstant_15 = OpTypePointer UniformConstant %15\n      %u_Tex = OpVariable %_ptr_UniformConstant_15 UniformConstant"
+    expected_assembly_substr = """%14 = OpTypeImage %float 2D 0 0 0 1 Unknown
+         %15 = OpTypeSampledImage %14
+%_ptr_UniformConstant_15 = OpTypePointer UniformConstant %15
+      %u_Tex = OpVariable %_ptr_UniformConstant_15 UniformConstant"""
 
