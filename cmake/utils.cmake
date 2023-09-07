9b5ad16550785af3f91753009f3a229cf20cad07
Tyler Kropp <tyler@tylerkropp.xyz>
2023-09-06 22:04:09 -0400

    Use TARGET_OBJECTS to replace archive acrobatics

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -173,50 +173,10 @@ endmacro()
 # Combines the static library "target" with all of its transitive static
 # library dependencies into a single static library "new_target".
 function(shaderc_combine_static_lib new_target target)
-
   set(all_libs "")
   shaderc_get_transitive_libs(${target} all_libs)
-
-  set(libname
-      ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${new_target}${CMAKE_STATIC_LIBRARY_SUFFIX})
-
-  if (MSVC)
-    string(REPLACE ";" ">;$<TARGET_FILE:" temp_string "${all_libs}")
-    set(lib_target_list "$<TARGET_FILE:${temp_string}>")
-
-    add_custom_command(OUTPUT ${libname}
-      DEPENDS ${all_libs}
-      COMMAND lib.exe ${lib_target_list} /OUT:${libname} /NOLOGO)
-  elseif(APPLE)
-    string(REPLACE ";" ">;$<TARGET_FILE:" temp_string "${all_libs}")
-    set(lib_target_list "$<TARGET_FILE:${temp_string}>")
-
-    add_custom_command(OUTPUT ${libname}
-      DEPENDS ${all_libs}
-      COMMAND libtool -static -o ${libname} ${lib_target_list})
-  else()
-    string(REPLACE ";" "> \naddlib $<TARGET_FILE:" temp_string "${all_libs}")
-    set(start_of_file
-      "create ${libname}\naddlib $<TARGET_FILE:${temp_string}>")
-    set(build_script_file "${start_of_file}\nsave\nend\n")
-
-    file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${new_target}.ar"
-        CONTENT ${build_script_file}
-        CONDITION 1)
-
-    add_custom_command(OUTPUT  ${libname}
-      DEPENDS ${all_libs}
-      COMMAND ${CMAKE_AR} -M < ${new_target}.ar)
-  endif()
-
-  add_custom_target(${new_target}_genfile ALL
-    DEPENDS ${libname})
-
-  # CMake needs to be able to see this as another normal library,
-  # so import the newly created library as an imported library,
-  # and set up the dependencies on the custom target.
-  add_library(${new_target} STATIC IMPORTED)
-  set_target_properties(${new_target}
-    PROPERTIES IMPORTED_LOCATION ${libname})
-  add_dependencies(${new_target} ${new_target}_genfile)
+  add_library(${new_target} STATIC)
+  foreach(lib IN LISTS all_libs)
+    target_sources(${new_target} PRIVATE $<TARGET_OBJECTS:${lib}>)
+  endforeach()
 endfunction()
