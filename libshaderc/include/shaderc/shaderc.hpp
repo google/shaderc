26e54f02d8ea2db771abb946065ea0c597641be8
Trice Helix <81759026+TriceHelix@users.noreply.github.com>
2024-05-20 14:56:30 +0200

    Fix comment typo in shaderc.hpp
    "Sets whether the compiler should generates code for max an min" -> "Sets whether the compiler should generate code for max and min"

--- libshaderc/include/shaderc/shaderc.hpp
+++ libshaderc/include/shaderc/shaderc.hpp
@@ -365,7 +365,7 @@ class CompileOptions {
     shaderc_compile_options_set_invert_y(options_, enable);
   }
 
-  // Sets whether the compiler should generates code for max an min which,
+  // Sets whether the compiler should generate code for max and min which,
   // if given a NaN operand, will return the other operand. Similarly, the
   // clamp builtin will favour the non-NaN operands, as if clamp were
   // implemented as a composition of max and min.
